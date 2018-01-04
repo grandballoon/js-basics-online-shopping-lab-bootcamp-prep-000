657ba1c3afec88ca9dde8d0cad30bc9363b8c770
Luke Conley <luke.sovern.conley@gmail.com>
2018-01-04 02:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
  if (cardNumber !== undefined) {
-   console.log(`Your total cost is ${sum}, which will be added to the card ${cardNumber}.`);
+   console.log(`Your total cost is ${sum}, which will be charged to the card ${cardNumber}.`);
  }
  else {
    console.log("Sorry, we don\'t have a credit card on file for you.");
